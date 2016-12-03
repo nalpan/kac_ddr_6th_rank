fddde7224a6a049439a25354cfb0b41fc67486b6
nalpan <nalpan@example.com>
2016-12-04 03:43:39 +0900

    add compression file

--- kac_ddr_6th_rank_v02.js
+++ kac_ddr_6th_rank_v02.js
@@ -1,1 +1,24 @@
-javascript:(function(){rows = document.querySelectorAll("table.normal-table>tbody>tr");first_scores = [];first_cols = rows[0].getElementsByClassName("score");for(col of first_cols){first_scores.push(Number(col.innerText.replace(",", "")));}for(row of rows){cols = row.getElementsByClassName("score");if(cols.length==0){continue;}for(i = 0; i<7; i++){score = cols[i].innerText.replace(",", "");diff = Number(score)-first_scores[i];diff_str = String(diff);if(diff >= 0){diff_str = "+"+diff;}cols[i].innerHTML = cols[i].innerText+'<br>(<span style="color:red">'+diff_str+'</span>)';}}})()
+javascript:(function(){
+rows = document.querySelectorAll("table.normal-table>tbody>tr");
+first_scores = [];
+first_cols = rows[0].getElementsByClassName("score");
+for(col of first_cols){
+    first_scores.push(Number(col.innerText.replace(",", "")));
+}
+for(row of rows){
+    cols = row.getElementsByClassName("score");
+    if(cols.length==0){
+        continue;
+    }
+    for(i = 0;i<7;i++){
+        score = cols[i].innerText.replace(",", "");
+        diff = Number(score)-first_scores[i];
+        diff_str = String(diff);
+        if(diff >= 0){
+            diff_str = "+"+diff;
+        }
+        cols[i].innerHTML =
+          cols[i].innerText+'<br>(<span style="color:red">'+diff_str+'</span>)';
+    }
+}
+})()
